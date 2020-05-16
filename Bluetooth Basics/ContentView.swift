2ab1b34ca43e5c60af99f6897e741581e697cd8d
Andino, Alvin <alvin.andino@accenture.com>
2020-05-16 12:36:49 -0500

    Initial Commit

---
+++ Bluetooth Basics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bluetooth Basics
+//
+//  Created by Alvin Andino on 5/16/20.
+//  Copyright © 2020 Alvin Andino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
